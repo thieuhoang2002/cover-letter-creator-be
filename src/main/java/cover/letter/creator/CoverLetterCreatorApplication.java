0cbca9a7a4305c259dba43c5e9124999c5d3f76d
Thieu Viet Hoang <thieuhoangent@gmail.com>
2025-03-15 18:16:34 +0700

    first commit

---
+++ src/main/java/cover/letter/creator/CoverLetterCreatorApplication.java
@@ -0,0 +1,13 @@
+package cover.letter.creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverLetterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverLetterCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cover/letter/creator/CoverLetterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cover.letter.creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverLetterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
